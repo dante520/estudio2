89f22ddf410a616beb1b6490bbd409e9d74e5c76
= <dantemcastillo23@gmail.com>
2020-09-17 10:38:57 -0500

    agregramos comentarios paso 1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
+var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
